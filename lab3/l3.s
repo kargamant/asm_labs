54c401a542d56e2ca0bfe5cf70f9e22b06800101
kargamant <firexholms@gmail.com>
2024-04-03 20:32:07 +0300

    Made a prog work for any length line. Fixed some buggs

--- lab3/l3.s
+++ lab3/l3.s
@@ -9,16 +9,22 @@ global _start
 
 _start:
 	call read
+	mov r9, rax
+	cmp rax, 0
+	je exit
 	dec rax
-	;dec rax
 	mov rdi, rax ;read length
 	mov rsi, 3 ;shift
 	call process
 	call write
+	jmp _start
 
+exit:
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+
 read:
 	mov rax, 0
 	mov rdi, 0
@@ -26,11 +32,15 @@ read:
 	mov rdx, 10
 	syscall
 	ret
+
+
 process:	
 	mov rcx, rdi
 cipher:
 	mov r8b, byte [buffin+rcx]
 	cmp r8b, 32
+	je continue
+	cmp r8b, 10
 	je continue
 	add r8b, sil ;shifting
 continue:
@@ -39,16 +49,21 @@ continue:
 	mov r8b, byte [buffin]
 	cmp r8b, 32
 	je process_end
+	cmp r8b, 10
+	je process_end
 	add r8b, sil
 	
 process_end:
 	mov [buffout], r8b
 	ret
+
+
 write:
-	mov byte [buffout+9], 10
+	;mov byte [buffout+r9], 10
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, buffout
-	mov rdx, 10
+	mov rdx, r9
+	;inc rdx
 	syscall
 	ret
